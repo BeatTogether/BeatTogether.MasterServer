391ba89f48c05decd559cff695eec4b997058359
cubic <cubicgraphics98@gmail.com>
2022-07-26 19:50:26 +0100

    Changes for api server

--- BeatTogether.MasterServer.Kernel/Abstractions/INodeRepository.cs
+++ BeatTogether.MasterServer.Kernel/Abstractions/INodeRepository.cs
@@ -1,5 +1,6 @@
 ﻿
 using BeatTogether.MasterServer.Domain.Models;
+using BeatTogether.MasterServer.Messaging.Enums;
 using System.Collections.Concurrent;
 using System.Net;
 using System.Threading.Tasks;
@@ -18,7 +19,7 @@ namespace BeatTogether.MasterServer.Kernal.Abstractions
         public void StartWaitForAllNodesTask();
         public bool EndpointExists(IPEndPoint endPoint);
 
-        Task<bool> SendAndAwaitPlayerEncryptionRecievedFromNode(IPEndPoint NodeEndPoint, EndPoint SessionEndPoint, string UserId, string UserName, byte[] Random, byte[] PublicKey, int TimeOut);
+        Task<bool> SendAndAwaitPlayerEncryptionRecievedFromNode(IPEndPoint NodeEndPoint, EndPoint SessionEndPoint, string UserId, string UserName, Platform platform, byte[] Random, byte[] PublicKey, int TimeOut);
         void OnNodeRecievedEncryptionParameters(IPEndPoint NodeEndPoint, EndPoint PlayerEndpoint);
     }
 }
