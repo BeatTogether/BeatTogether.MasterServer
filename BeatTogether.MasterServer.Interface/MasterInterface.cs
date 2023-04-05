2ace0d06fb9bf4a1e842f72c285c88b4d7122ea7
cubic <cubicgraphics98@gmail.com>
2023-04-05 01:46:29 +0100

    Should fix interface issue

---
+++ BeatTogether.MasterServer.Interface/MasterInterface.cs
@@ -0,0 +1,21 @@
+﻿using Autobus;
+using BeatTogether.MasterServer.Interface.Events;
+
+namespace BeatTogether.MasterServer.Interface
+{
+    public class MasterInterfaceService
+    {
+
+
+        public class MasterServerServiceContract : BaseServiceContract
+        {
+            public override void Build(IServiceContractBuilder builder) =>
+                builder
+                    .UseName("MasterServer")
+                    .AddEvent<CheckNodesEvent>()
+                    .AddEvent<DisconnectPlayerFromMatchmakingServerEvent>()
+                    .AddEvent<PlayerConnectedToMatchmakingServerEvent>()
+                    .AddEvent<CloseServerInstanceEvent>();
+        }
+    }
+}
